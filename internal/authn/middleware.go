695e688744f5e17bea782398412422ae93174d7e
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-06-30 06:26:31 -0400

    Route DPoP binding through verifier

--- internal/authn/middleware.go
+++ internal/authn/middleware.go
@@ -53,9 +53,6 @@ func preflightRequest(verifier *DPoPVerifier, r *http.Request) error {
 			return errInvalidAuthorization("DPoP authorization requires DPoP proof header")
 		}
 		if hasProof {
-			if err := ConfirmDPoPTokenBinding(token, dpopProof); err != nil {
-				return err
-			}
 			return verifier.VerifyRequest(r, token, dpopProof)
 		}
 		return nil
